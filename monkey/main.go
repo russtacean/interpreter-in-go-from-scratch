46e0bf0ec1a7e903900b6992ad83d3f3873e417e
Russ Gunther <russell.g@leantaas.com>
2024-04-01 16:37:05 -0400

    Chap 1.5: Create start of REPL, use it to print tokens

--- monkey/main.go
+++ monkey/main.go
@@ -1,4 +1,18 @@
 package main
 
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
 func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s, this is the Monkey programming language\n", user)
+	fmt.Printf("Feel free to start typing commands")
+	repl.Start(os.Stdin, os.Stdout)
 }

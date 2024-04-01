52b846765a3ee93c8df3fd21874a7499d569b58c
Russ Gunther <russell.g@leantaas.com>
2024-03-31 21:52:35 -0400

    Chap 1.3: Add keywords, identifiers and functions to lexer

--- monkey/lexer/lexer.go
+++ monkey/lexer/lexer.go
@@ -30,6 +30,8 @@ func (lexer *Lexer) readChar() {
 func (lexer *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	lexer.skipWhitespace()
+
 	switch lexer.ch {
 	case '=':
 		tok = newToken(token.ASSIGN, lexer.ch)
@@ -50,12 +52,54 @@ func (lexer *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(lexer.ch) {
+			tok.Literal = lexer.readIdentifier()
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit(lexer.ch) {
+			tok.Literal = lexer.readNumber()
+			tok.Type = token.INT
+			return tok
+		} else {
+			tok = newToken(token.ILLEGAL, lexer.ch)
+		}
 	}
 
 	lexer.readChar()
 	return tok
 }
 
+func (lexer *Lexer) readIdentifier() string {
+	position := lexer.position
+	for isLetter(lexer.ch) {
+		lexer.readChar()
+	}
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) readNumber() string {
+	position := lexer.position
+	for isDigit(lexer.ch) {
+		lexer.readChar()
+	}
+	return lexer.input[position:lexer.position]
+}
+
+func (lexer *Lexer) skipWhitespace() {
+	for lexer.ch == ' ' || lexer.ch == '\t' || lexer.ch == '\n' || lexer.ch == '\r' {
+		lexer.readChar()
+	}
+}
+
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
--- monkey/lexer/lexer_test.go
+++ monkey/lexer/lexer_test.go
@@ -5,24 +5,12 @@ import (
 	"testing"
 )
 
-func TestNextToken(t *testing.T) {
-	input := `=+(){},;`
+type NextTokenTest struct {
+	expectedType    token.TokenType
+	expectedLiteral string
+}
 
-	tests := []struct {
-		expectedType    token.TokenType
-		expectedLiteral string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
-	}
-
+func verifyNextToken(t *testing.T, input string, tests []NextTokenTest) {
 	l := New(input)
 
 	for i, tt := range tests {
@@ -38,4 +26,76 @@ func TestNextToken(t *testing.T) {
 				i, tt.expectedLiteral, tok.Literal)
 		}
 	}
+}
+
+func TestNextTokenBasicTokens(t *testing.T) {
+	input := `=+(){},;`
+
+	tests := []NextTokenTest{
+		{token.ASSIGN, "="},
+		{token.PLUS, "+"},
+		{token.LPAREN, "("},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RBRACE, "}"},
+		{token.COMMA, ","},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	verifyNextToken(t, input, tests)
+}
+
+func TestNextTokenAssignmentAndFns(t *testing.T) {
+	input := `let five = 5;
+	let ten = 10;
+	
+	let add = fn(x, y) {
+	  x + y;
+	};
+
+	let result = add(five, ten);
+	`
+
+	tests := []NextTokenTest{
+		{token.LET, "let"},
+		{token.IDENT, "five"},
+		{token.ASSIGN, "="},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "ten"},
+		{token.ASSIGN, "="},
+		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "add"},
+		{token.ASSIGN, "="},
+		{token.FUNCTION, "fn"},
+		{token.LPAREN, "("},
+		{token.IDENT, "x"},
+		{token.COMMA, ","},
+		{token.IDENT, "y"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.IDENT, "x"},
+		{token.PLUS, "+"},
+		{token.IDENT, "y"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.SEMICOLON, ";"},
+		{token.LET, "let"},
+		{token.IDENT, "result"},
+		{token.ASSIGN, "="},
+		{token.IDENT, "add"},
+		{token.LPAREN, "("},
+		{token.IDENT, "five"},
+		{token.COMMA, ","},
+		{token.IDENT, "ten"},
+		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.EOF, ""},
+	}
+
+	verifyNextToken(t, input, tests)
 }

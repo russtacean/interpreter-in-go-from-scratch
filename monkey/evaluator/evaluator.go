fbf8a248f3d077efbfe2808c9067e7205fc1d29f
Russ Gunther <russell.g@leantaas.com>
2024-04-06 18:06:20 -0400

    Chap 4.2: Add eval support for string literals

--- monkey/evaluator/evaluator.go
+++ monkey/evaluator/evaluator.go
@@ -41,6 +41,9 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 	case *ast.BooleanLiteral:
 		return nativeBoolToBooleanObject(node.Value)
 
+	case *ast.StringLiteral:
+		return &object.String{Value: node.Value}
+
 	case *ast.Identifier:
 		return evalIdentifier(node, env)
 

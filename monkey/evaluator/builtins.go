e7f2bc788430c0a7e5680747f475482ba0b7ebbd
Russ Gunther <russell.g@leantaas.com>
2024-04-07 12:54:02 -0400

    Chap 4.6: Add built-in func puts() to allow printing to stdout

--- monkey/evaluator/builtins.go
+++ monkey/evaluator/builtins.go
@@ -1,6 +1,9 @@
 package evaluator
 
-import "monkey/object"
+import (
+	"fmt"
+	"monkey/object"
+)
 
 var builtins = map[string]*object.Builtin{
 	"len": &object.Builtin{
@@ -97,6 +100,15 @@ var builtins = map[string]*object.Builtin{
 			default:
 				return newError("argument to `rest` not supported, got %s", args[0].Type())
 			}
+		},
+	},
+	"puts": &object.Builtin{
+		Fn: func(args ...object.Object) object.Object {
+			for _, arg := range args {
+				fmt.Println(arg.Inspect())
+			}
+
+			return NULL
 		},
 	},
 }

67479927a2e2e1f156cdcb48aba2203c30d50816
Russ Gunther <russell.g@leantaas.com>
2024-04-13 17:55:15 -0400

    Chap 8: Add compiler support for simple if statement with no else

--- monkey/compiler/compiler.go
+++ monkey/compiler/compiler.go
@@ -8,20 +8,42 @@ import (
 )
 
 type Compiler struct {
-	instructions code.Instructions
-	constants    []object.Object
+	instructions           code.Instructions
+	constants              []object.Object
+	lastInstruction        EmittedInstruction
+	penultimateInstruction EmittedInstruction
+}
+
+type Bytecode struct {
+	Instructions code.Instructions
+	Constants    []object.Object
+}
+
+type EmittedInstruction struct {
+	Opcode   code.Opcode
+	Position int
 }
 
 func New() *Compiler {
 	return &Compiler{
-		instructions: code.Instructions{},
-		constants:    []object.Object{},
+		instructions:           code.Instructions{},
+		constants:              []object.Object{},
+		lastInstruction:        EmittedInstruction{},
+		penultimateInstruction: EmittedInstruction{},
 	}
 }
 
 func (compiler *Compiler) Compile(node ast.Node) error {
 	switch node := node.(type) {
 	case *ast.Program:
+		for _, s := range node.Statements {
+			err := compiler.Compile(s)
+			if err != nil {
+				return err
+			}
+		}
+
+	case *ast.BlockStatement:
 		for _, s := range node.Statements {
 			err := compiler.Compile(s)
 			if err != nil {
@@ -95,6 +117,26 @@ func (compiler *Compiler) Compile(node ast.Node) error {
 			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 
+	case *ast.IfExpression:
+		err := compiler.Compile(node.Condition)
+		if err != nil {
+			return err
+		}
+
+		// Emit OpJumpNotTruthy with bogus value, we will backpatch later
+		jumpNotTruthyPos := compiler.emit(code.OpJumpNotTruthy, 9999)
+
+		err = compiler.Compile(node.Consequence)
+		if err != nil {
+			return err
+		}
+
+		if compiler.isLastInstructionPop() {
+			compiler.removeLastPop()
+		}
+		afterConsequencePos := len(compiler.instructions)
+		compiler.changeOperand(jumpNotTruthyPos, afterConsequencePos)
+
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
 		compiler.emit(code.OpConstant, compiler.addConstant(integer))
@@ -115,10 +157,19 @@ func (compiler *Compiler) addConstant(obj object.Object) int {
 	return len(compiler.constants) - 1
 }
 
-func (compiler *Compiler) emit(op code.Opcode, operands ...int) int {
-	ins := code.Make(op, operands...)
+func (compiler *Compiler) emit(opcode code.Opcode, operands ...int) int {
+	ins := code.Make(opcode, operands...)
 	pos := compiler.addInstruction(ins)
+	compiler.setLastInstruction(opcode, pos)
 	return pos
+}
+
+func (compiler *Compiler) setLastInstruction(opcode code.Opcode, position int) {
+	penultimate := compiler.lastInstruction
+	last := EmittedInstruction{Opcode: opcode, Position: position}
+
+	compiler.lastInstruction = last
+	compiler.penultimateInstruction = penultimate
 }
 
 func (compiler *Compiler) addInstruction(ins []byte) int {
@@ -127,14 +178,31 @@ func (compiler *Compiler) addInstruction(ins []byte) int {
 	return posNewInstruction
 }
 
+func (compiler *Compiler) isLastInstructionPop() bool {
+	return compiler.lastInstruction.Opcode == code.OpPop
+}
+
+func (compiler *Compiler) removeLastPop() {
+	compiler.instructions = compiler.instructions[:compiler.lastInstruction.Position]
+	compiler.lastInstruction = compiler.penultimateInstruction
+}
+
+func (compiler *Compiler) replaceInstruction(pos int, newInstruction []byte) {
+	for i := 0; i < len(newInstruction); i++ {
+		compiler.instructions[i+1] = newInstruction[i]
+	}
+}
+
+func (compiler *Compiler) changeOperand(opPos int, operand int) {
+	opcode := code.Opcode(compiler.instructions[opPos])
+	newInstruction := code.Make(opcode, operand)
+	compiler.replaceInstruction(opPos, newInstruction)
+
+}
+
 func (compiler *Compiler) Bytecode() *Bytecode {
 	return &Bytecode{
 		Instructions: compiler.instructions,
 		Constants:    compiler.constants,
 	}
-}
-
-type Bytecode struct {
-	Instructions code.Instructions
-	Constants    []object.Object
 }

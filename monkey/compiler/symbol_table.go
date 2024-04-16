0440ef8749fe1683093ee321c59de74d8d10c803
Russ Gunther <russell.g@leantaas.com>
2024-04-16 18:27:08 -0400

    Chap 11: Support scoping levels in the symbol table

--- monkey/compiler/symbol_table.go
+++ monkey/compiler/symbol_table.go
@@ -4,6 +4,7 @@ type SymbolScope string
 
 const (
 	GlobalScope SymbolScope = "GLOBAL"
+	LocalScope  SymbolScope = "LOCAL"
 )
 
 type Symbol struct {
@@ -13,6 +14,7 @@ type Symbol struct {
 }
 
 type SymbolTable struct {
+	Outer          *SymbolTable // Similar to outer env in tree walking interpreter
 	store          map[string]Symbol
 	numDefinitions int
 }
@@ -22,8 +24,20 @@ func NewSymbolTable() *SymbolTable {
 	return &SymbolTable{store: store}
 }
 
+func NewEnclosedSymbolTable(outer *SymbolTable) *SymbolTable {
+	s := NewSymbolTable()
+	s.Outer = outer
+	return s
+}
+
 func (st *SymbolTable) Define(name string) Symbol {
-	symbol := Symbol{Name: name, Index: st.numDefinitions, Scope: GlobalScope}
+	symbol := Symbol{Name: name, Index: st.numDefinitions}
+	if st.Outer == nil {
+		symbol.Scope = GlobalScope
+	} else {
+		symbol.Scope = LocalScope
+	}
+
 	st.store[name] = symbol
 	st.numDefinitions++
 	return symbol
@@ -31,5 +45,9 @@ func (st *SymbolTable) Define(name string) Symbol {
 
 func (st *SymbolTable) Resolve(name string) (Symbol, bool) {
 	symbol, ok := st.store[name]
+	if !ok && st.Outer != nil {
+		symbol, ok = st.Outer.Resolve(name)
+		return symbol, ok
+	}
 	return symbol, ok
 }

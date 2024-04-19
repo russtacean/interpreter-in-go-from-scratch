e198a72728590c3017c68ca42b7d469c37a2e456
Russ Gunther <russell.g@leantaas.com>
2024-04-18 21:43:50 -0400

    Chap 13: Push current frame's closure onto stack to execute recursive functions defined in other functions

--- monkey/vm/vm.go
+++ monkey/vm/vm.go
@@ -269,6 +269,12 @@ func (vm *VM) Run() error {
 				return err
 			}
 
+		case code.OpCurrentClosure:
+			currentClosure := vm.currentFrame().closure
+			err := vm.push(currentClosure)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
